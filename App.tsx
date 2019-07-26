4a9bb8760b9de5d405ae7912344270e9eb64d1e1
Sergey Tskhovrebov <sinxwal@gmail.com>
2019-07-26 17:02:24 +0300

    Init ReactNativeDeveloperChallenge app

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, { Fragment } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Vending machine</Text>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+export default App;
